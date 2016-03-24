d394de5c65ddd48658cb90cca12502b4976233d7
Robin Bygrave <robin.bygrave@gmail.com>
2016-03-24 16:46:32 +1300

    Initial add

---
+++ src/main/java/org/avaje/classpath/scanner/ClassPathScannerFactory.java
@@ -0,0 +1,12 @@
+package org.avaje.classpath.scanner;
+
+/**
+ * Factory for the ClassPathScanner service.
+ */
+public interface ClassPathScannerFactory {
+
+  /**
+   * Create a ClassPathScanner given the classLoader.
+   */
+  ClassPathScanner createScanner(ClassLoader classLoader);
+}
---
+++ src/test/java/org/avaje/classpath/scanner/FilterResourceTest.java
@@ -0,0 +1,85 @@
+package org.avaje.classpath.scanner;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FilterResourceTest {
+
+  @Test
+  public void byPrefixSuffix_when_match() throws Exception {
+
+    ResourceFilter match = FilterResource.byPrefixSuffix("oe_", ".txt");
+
+    assertTrue(match.isMatch("oe_.txt"));
+    assertTrue(match.isMatch("oe_123.txt"));
+    assertTrue(match.isMatch("oe_a.txt"));
+    assertTrue(match.isMatch("oe_a.foo.txt"));
+  }
+
+  @Test
+  public void byPrefixSuffix_when_notMatch() throws Exception {
+
+    ResourceFilter match = FilterResource.byPrefixSuffix("oe_", ".txt");
+
+    assertFalse(match.isMatch("e_.txt"));
+    assertFalse(match.isMatch("o_.txt"));
+    assertFalse(match.isMatch("oe.txt"));
+    assertFalse(match.isMatch("oe_txt"));
+    assertFalse(match.isMatch("oe_.xt"));
+    assertFalse(match.isMatch("oe_.tt"));
+    assertFalse(match.isMatch("oe_.tx"));
+  }
+
+  @Test
+  public void bySuffix_when_match() throws Exception {
+
+    ResourceFilter match = FilterResource.bySuffix(".txt");
+
+    assertTrue(match.isMatch(".txt"));
+    assertTrue(match.isMatch("a.txt"));
+    assertTrue(match.isMatch("_.txt"));
+    assertTrue(match.isMatch("1.txt"));
+    assertTrue(match.isMatch("1..foo.txt"));
+  }
+
+  @Test
+  public void bySuffix_when_notMatch() throws Exception {
+
+    ResourceFilter match = FilterResource.bySuffix(".txt");
+
+    assertFalse(match.isMatch("txt"));
+    assertFalse(match.isMatch(".xt"));
+    assertFalse(match.isMatch(".tt"));
+    assertFalse(match.isMatch(".tx"));
+    assertFalse(match.isMatch(".sql"));
+  }
+
+  @Test
+  public void byPrefix() throws Exception {
+
+    ResourceFilter match = FilterResource.byPrefix("a.b");
+
+    assertTrue(match.isMatch("a.b"));
+    assertTrue(match.isMatch("a.b."));
+    assertTrue(match.isMatch("a.ba"));
+    assertTrue(match.isMatch("a.b_"));
+    assertTrue(match.isMatch("a.b1"));
+    assertTrue(match.isMatch("a.b.txt"));
+  }
+
+  @Test
+  public void byPrefix_notMatch() throws Exception {
+
+    ResourceFilter match = FilterResource.byPrefix("a.b");
+
+    assertFalse(match.isMatch(".b"));
+    assertFalse(match.isMatch("ab"));
+    assertFalse(match.isMatch("a."));
+    assertFalse(match.isMatch(""));
+    assertFalse(match.isMatch(".a.b"));
+    assertFalse(match.isMatch("/a.b"));
+    assertFalse(match.isMatch("aa.b"));
+    assertFalse(match.isMatch("1a.b"));
+  }
+}
