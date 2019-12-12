d1dcd4edfb3aa43dbb77000e451da496b8fbcfe5
rob bygrave <robin.bygrave@gmail.com>
2019-12-13 12:19:24 +1300

    #4 Change groupId to io.avaje, bump to 4.1, change to junit

--- src/test/java/org/avaje/classpath/scanner/FilterResourceTest.java
+++ src/test/java/org/avaje/classpath/scanner/FilterResourceTest.java
@@ -1,9 +1,9 @@
 package org.avaje.classpath.scanner;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FilterResourceTest {
 
